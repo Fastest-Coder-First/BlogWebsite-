f0bf365c58ae4722376dfb7447c1738a6c4922ce
Neha <129548970+Neha5679@users.noreply.github.com>
2023-06-17 21:04:57 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,10 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navList=document.querySelector('.nav-list')
+rightnav=document.querySelector('.rightNav')
+
+burger.addEventListener('click',()=>{
+      rightNav.classList.toggle('v-class-resp')
+      nav-list.classList.toggle('v-class-resp')
+      navbar.classList.toggle('h-nav-resp')
+})
